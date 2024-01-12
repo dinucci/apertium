0e40914e87d5bacc78a5a15b6a00aba619f9e347
Ale DN <dinucci@protonmail.ch>
2024-01-12 05:04:15 +0100

    Update 00-install-arch.sh

--- 00-install-arch.sh
+++ 00-install-arch.sh
@@ -2,4 +2,6 @@
 sudo pacman -Sy
 
 # Installs Apertium development package and language packs for Spanish, English, Portuguese, Croatian, and Japanese
-sudo pacman -S apertium-all-dev apertium-es apertium-en apertium-pt apertium-hr apertium-ja
+#sudo pacman -S apertium-all-dev apertium-es apertium-en apertium-pt apertium-hr apertium-ja
+
+sudo pacman -Syy apertium apertium-spa apertium-eng apertium-es-pt apertium-fr-es apertium-hbs apertium-hbs-eng apertium-hbs-mkd apertium-hbs-slv apertium-ita apertium-mkd-eng apertium-pol apertium-por apertium-recursive apertium-regtest 
