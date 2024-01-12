148076b5a103fae3a280f12cff236bae38a40947
Ale DN <dinucci@protonmail.ch>
2024-01-12 05:18:32 +0100

    Create 00-install-ubuntu.sh

---
+++ 00-install-ubuntu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt-get update
+apt-get install apertium apertium-dev apertium-spa apertium-spa-ita apertium-en-es apertium-es-pt apertium-hbs-mkd apertium-hbs-mkd apertium-hbs-slv apertium-recursive python3-apertium-core python3-streamparser
